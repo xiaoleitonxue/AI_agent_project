916820c956f73c2a3fff8c2a862903cd9423a992
31286 <3128607571@qq.com>
2026-05-08 10:21:13 +0800

    feat(utils): 添加配置文件处理器和文件操作工具
    - 实现了配置文件加载功能，支持RAG、Chroma、Prompts和Agent配置
    - 创建了文件MD5计算和目录遍历功能
    - 添加了PDF和TXT文档加载器
    - 集成了主提示词、RAG摘要和报告生成提示词模板
    - 配置了模型名称和嵌入模型设置

---
+++ utils/file_handler.py
@@ -0,0 +1,47 @@
+import os
+import hashlib
+from utils.logger_handler import logger
+from langchain_core.documents import Document
+from langchain_community.document_loaders import TextLoader, PyPDFLoader
+
+def get_file_md5_hex(filepath: str):
+    if not os.path.exists(filepath):
+        logger.error(f"[md5计算]文件{filepath}不存在")
+        return
+
+    if os.path.isdir(filepath):
+        logger.error(f"[md5计算]路径{filepath}不是文件")
+        return
+
+    md5_obj = hashlib.md5()
+
+    chunk_size = 4096
+    try:
+        with open(filepath, "rb") as f:
+            while chunk := f.read(chunk_size):
+                md5_obj.update(chunk)
+            md5_hex = md5_obj.hexdigest()
+            return md5_hex
+    except Exception as e:
+        logger.error(f"[md5计算]文件{filepath}读取错误: {e}")
+        return
+
+def listdir_with_allowed_type(path: str, allowed_types: tuple[str]):
+    files = []
+
+    if not os.path.isdir(path):
+        logger.error(f"[listdir_with_allowed_type]路径{path}不是目录")
+        return allowed_types
+
+    for f in os.listdir(path):
+        if f.endswith(allowed_types):
+            files.append(os.path.join(path, f))
+
+    return tuple(files)
+
+
+def pdf_loader(filepath: str, passwd: str = None) -> list[Document]:
+    return PyPDFLoader(filepath, passwd).load()
+
+def txt_loader(filepath: str) -> list[Document]:
+    return TextLoader(filepath).load()
